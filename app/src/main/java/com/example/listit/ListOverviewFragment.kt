80bc1e3700a1644ad4ebc3d3b8e80d11d51e850d
Marie Kristine Ringdal <mariekr@uia.no>
2021-03-16 20:27:26 +0100

    Added functionality for adding and deleting lists

--- app/src/main/java/com/example/listit/ListOverviewFragment.kt
+++ app/src/main/java/com/example/listit/ListOverviewFragment.kt
@@ -1,23 +1,33 @@
 package com.example.listit
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listit.databinding.FragmentListOverviewBinding
 import com.example.listit.data.List
 import com.example.listit.data.ListOverviewRecyclerAdapter
+import kotlinx.android.synthetic.main.fragment_list_overview.view.*
 
 class ListOverviewFragment : Fragment() {
 
+    private val TAG:String = "listit.ListOverviewFragment"
     private var _binding:FragmentListOverviewBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var listAdapter:ListOverviewRecyclerAdapter
 
-    private var listOverview:MutableList<List> = mutableListOf(
+    private var listOverview:MutableList<List> =  mutableListOf(
+        List("Huskeliste", 50),
+        List("Handleliste", 25),
+        List("Pakkeliste", 80),
+        List("Hundeleker", 58),
         List("Huskeliste", 50),
         List("Handleliste", 25),
         List("Pakkeliste", 80),
@@ -37,12 +47,42 @@ class ListOverviewFragment : Fragment() {
         listAdapter = ListOverviewRecyclerAdapter(listOverview)
         binding.listsRecyclerView.adapter = listAdapter
 
+        view.addNewListButton.setOnClickListener {
+            addNewListDialog()
+        }
+
+
         return view
     }
 
-    // TODO: Add new list function
+    private fun addNewListDialog(){
+        val alertDialogBuilder = AlertDialog.Builder(context)
+        val alertDialogView = layoutInflater.inflate(R.layout.add_new_list, null)
+        val newListTitle = alertDialogView.findViewById<EditText>(R.id.newListTitleEditText)
 
-    // TODO: Add delete list function
+        with(alertDialogBuilder){
+            setTitle("Add a new list")
+            setView(alertDialogView)
 
+            setPositiveButton("Add"){_, _ ->
+                val listTitle = newListTitle.text.toString()
+                if (listTitle.isNotEmpty()){
+                    val list = List(listTitle, 0)
+                    listAdapter.addList(list)
+                    Toast.makeText(activity, "$listTitle was added",Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "User added a new list successfully")
+                } else{
+                    Toast.makeText(activity, "Give your list a name :)",Toast.LENGTH_SHORT).show()
+                }
 
+            }
+            setNegativeButton("Cancel"){dialog, _ ->
+                dialog.cancel()
+                Log.d(TAG, "User cancelled adding list")
+            }
+
+            create()
+            show()
+        }
+    }
 }
--- app/src/main/java/com/example/listit/data/ListOverviewRecyclerAdapter.kt
+++ app/src/main/java/com/example/listit/data/ListOverviewRecyclerAdapter.kt
@@ -1,29 +1,71 @@
 package com.example.listit.data
 
+import android.app.AlertDialog
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listit.R
 import com.example.listit.databinding.ListItemBinding
 
-class ListOverviewRecyclerAdapter(private val lists:MutableList<List>):
-    RecyclerView.Adapter<ListOverviewRecyclerAdapter.ViewHolder>(){
+class ListOverviewRecyclerAdapter(private val lists:MutableList<List>) :
+    RecyclerView.Adapter<ListOverviewRecyclerAdapter.ListViewHolder>(){
 
-    class ViewHolder(val binding: ListItemBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(list: List) {
+    private val TAG:String = "listit.ListOverviewRecyclerAdapter.kt"
+
+    inner class ListViewHolder(val binding: ListItemBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(list: List){
             binding.listTitle.text = list.title
             binding.listProgression.progress = list.progression
         }
 
+        init {
+            val deleteListButton = binding.root.findViewById<ImageButton>(R.id.deleteListButton)
+            deleteListButton.setOnClickListener {
+                deleteListDialog()
+                Log.d(TAG, "Delete list button pressed")
+            }
+        }
+
+        private fun deleteListDialog(){
+            val alertDialogBuilder = AlertDialog.Builder(binding.root.context)
+
+            with(alertDialogBuilder){
+                setTitle("Delete")
+                setMessage("Are you sure?")
+
+                setPositiveButton("Delete"){dialog, _ ->
+                    lists.removeAt(adapterPosition)
+                    notifyItemRemoved(adapterPosition)
+                    notifyDataSetChanged()
+                    Toast.makeText(context, "Deleted ", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+                setNegativeButton("Cancel"){dialog, _ ->
+                    dialog.cancel()
+                }
+
+                create()
+                show()
+            }
+
+        }
     }
 
     override fun getItemCount(): Int = lists.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val list = lists[position]
-        holder.bind(list)
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val currentList = lists[position]
+        holder.bind(currentList)
+    }
+
+    fun addList(list: List){
+        lists.add(list)
+        notifyItemInserted(lists.size - 1)
     }
 }
